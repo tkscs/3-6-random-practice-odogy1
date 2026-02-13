22fa796dc67199a478c1cdeecb494e0a13fc30f5
odogy1 <ocharrette2028@kehillahstudent.org>
2026-02-13 11:37:59 -0800

    Final

--- q2.py
+++ q2.py
@@ -1,14 +1,16 @@
+import random
+
 # Make a random pet.
 
 # Choose:
 # Type of animal (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
+animal = (["dog", "cat", "fish"])
 # Age (integer)
-age = None#REPLACE THIS WITH YOUR CODE
+age = random.randint(1,15)
 # Color (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
+color = (["red", "blue", "golden", "white", "orange", "black", "green"])
 # Weight (float)
-weight = None#REPLACE THIS WITH YOUR CODE
+weight = (["small", "average sized", "large", "fat", "skinny"])
 
 # Print a summary of your pet using an f-string
-print(f"Your pet is a {animal}")#REPLACE THIS WITH YOUR CODE
+print(f"Your pet is a {age} year old {random.choices(color)}, {random.choices(weight)}, {random.choices(animal)}")#REPLACE THIS WITH YOUR CODE
